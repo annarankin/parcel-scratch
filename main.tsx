4eed80e9fa5ffe19c4be9bfbfa49324815806bc9
Anna Rankin <anna@annarankin.com>
2018-07-20 13:34:27 -0400

    Adds stepper

--- main.tsx
+++ main.tsx
@@ -1,16 +1,48 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-import { Button } from 'antd'
+import { Button, Steps } from 'antd'
+const Step = Steps.Step;
 
 import './main.less'
 import 'antd/dist/antd.css'
 
-const Greeting: React.SFC<{ hello: string }> = ({ hello }) => (
-  <div style={{ padding: '20px' }}>
-    <Button>{hello}</Button>
-  </div>
-)
+type StepperState = { currentStep: number }
+
+class Stepper extends React.Component {
+  state: StepperState = { currentStep: 0 }
+
+  incrementStep = () => {
+    this.setState((prevState: StepperState): StepperState => {
+      const nextStep: number = Math.min(prevState.currentStep + 1, 2)
+      return { currentStep: nextStep }
+    })
+  }
+
+  decrementStep = () => {
+    this.setState((prevState: StepperState): StepperState => {
+      const prevStep: number = Math.max(prevState.currentStep - 1, 0)
+      return { currentStep: prevStep }
+    })
+  }
+
+  render() {
+    const { currentStep } = this.state
+
+    return(
+      <div style={{ padding: '20px' }}>
+        <Steps direction="vertical" current={currentStep}>
+          <Step title="Finished" description="This is a description." />
+          <Step title="In Progress" description="This is a description." />
+          <Step title="Waiting" description="This is a description." />
+        </Steps>
+
+        <Button onClick={this.decrementStep} style={{ marginRight: '20px'}}>Previous</Button>
+        <Button onClick={this.incrementStep}>Next</Button>
+      </div>
+    )
+  }
+}
 
 const rootNode: HTMLDivElement = document.querySelector('#app')
 
-render(<Greeting hello="Howday" />, rootNode)
+render(<Stepper />, rootNode)
